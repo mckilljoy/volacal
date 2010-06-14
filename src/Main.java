248d0a67a4a5e11df2974ea27b2c896a004fc59c
Marty Greenia <marty.greeni@gmail.com>
2010-06-14 10:41:04 -0700

    basic files added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import volacal.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Running main"); //Display the string.
+
+        Volacal vol = new Volacal();
+
+        vol.Version();
+    }
+}
---
+++ src/volacal/Volacal.java
@@ -0,0 +1,14 @@
+package volacal;
+
+public class Volacal {
+    private double version = 0.1;
+
+    public Volacal() {
+
+    }
+
+    public void Version() {
+        System.out.println("Volacal version " + this.version); //Display the string.        
+    }
+ 
+}
