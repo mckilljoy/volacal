98f82e80e6be59f5e5b50d9e3a704ee5b1192ad0
Marty Greenia <marty.greeni@gmail.com>
2010-06-16 00:08:12 +0000

    will run either the class files or the jar

--- volacal.sh
+++ volacal.sh
@@ -1,1 +1,11 @@
-java -cp classes/ Main
+#!/bin/bash
+
+if [ -e volacal.jar ]; then
+	echo "Using jar file"
+	java -jar volacal.jar
+elif [ -e classes/Main.class ]; then
+	echo "Using class files directly"
+	java -cp classes/ Main
+else 
+	echo "Nothing to run"
+fi
